02a6f60928cea6f9c889ff1422e89843e3ba6533
Luc Forget <luc.forget@insa-lyon.fr>
2022-03-16 14:52:13 -0700

    UDL for bitint

--- include/bitint_tools/type_helpers.hpp
+++ include/bitint_tools/type_helpers.hpp
@@ -1,6 +1,9 @@
 #ifndef BITINT_TOOLS_TYPE_HELPERS_HPP
 #define BITINT_TOOLS_TYPE_HELPERS_HPP
 
+#include <algorithm>
+#include <string_view>
+
 namespace hint {
 namespace detail {
 
@@ -40,5 +43,39 @@ concept BitIntT = SignedBitIntT<T> || UnsignedBitIntT<T>;
 template <typename T> constexpr bool signedType = false;
 
 template <SignedBitIntT T> constexpr bool signedType<T> = true;
+
+namespace detail {
+template<char... Bits>
+struct BitIntUDLHelper{
+  static constexpr char tab[]{Bits..., '\0'};
+  static constexpr std::string_view prefixed_bits{tab};
+  static_assert(prefixed_bits.starts_with("0b") || prefixed_bits.starts_with("0B"), "Only binary litterals are accepted");
+  static constexpr auto bits = prefixed_bits.substr(2);
+  static constexpr auto nb_sep = std::count(bits.begin(), bits.end(), '\'');
+  static constexpr auto width = bits.size() - nb_sep;
+
+  template<bool isSigned>
+  static constexpr bitint_base_t<isSigned, width> get_value() {
+    bitint_base_t<isSigned, width> result{0};
+    for (auto c : bits) {
+      if (c == '\'') continue;
+      result <<= 1;
+      if (c == '1') result += 1;
+    }
+    return result;
+  };
+};
+}
+
+template<char... Bits>
+constexpr auto operator ""_ubi() { //Unsigned bit int
+  return detail::BitIntUDLHelper<Bits...>::template get_value<false>();
+}
+
+template<char... Bits>
+constexpr auto operator ""_sbi() { //Signed bit int
+  return detail::BitIntUDLHelper<Bits...>::template get_value<true>();
+}
+
 } // namespace hint
 #endif
--- tests/bitint_tools/bitint_tests.cpp
+++ tests/bitint_tools/bitint_tests.cpp
@@ -7,10 +7,29 @@
 #include <boost/test/unit_test.hpp>
 
 #include "bitint_tools/bitint_constant.hpp"
+#include "bitint_tools/type_helpers.hpp"
 
 BOOST_AUTO_TEST_CASE(TestFit) {
   constexpr unsigned _BitInt(32) testVal{0x3BACD};
   constexpr auto cst = hint::fitted_c<testVal>;
   static_assert(cst.width == 18, "Incorrect fitting");
   static_assert(cst.value == 0x3bacd, "Incorrect value");
+}
+
+
+BOOST_AUTO_TEST_CASE(TestUDL) {
+  using hint::operator ""_ubi;
+  using hint::operator ""_sbi;
+
+  constexpr auto c = 0b1101_ubi;
+  using utype = decltype(c);
+  using target_type = unsigned _BitInt(4);
+  static_assert(std::is_same_v<std::remove_cvref_t<utype>, target_type>);
+  static_assert( c == 13);
+
+  constexpr auto d = 0b1101_sbi;
+  using stype = decltype(d);
+  using target_stype = signed _BitInt(4);
+  static_assert(std::is_same_v<std::remove_cvref_t<stype>, target_stype>);
+  static_assert(d == -3);
 }
